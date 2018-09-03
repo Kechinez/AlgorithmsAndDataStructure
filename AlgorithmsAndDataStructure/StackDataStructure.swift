5604ea90935a86b785dfe50015be5c1b0fa83513
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-09-03 11:57:34 +0300

    init commit

---
+++ AlgorithmsAndDataStructure/StackDataStructure.swift
@@ -0,0 +1,9 @@
+//
+//  StackDataStructure.swift
+//  AlgorithmsAndDataStructure
+//
+//  Created by Nikita Kechinov on 03.09.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import Foundation
---
+++ AlgorithmsAndDataStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmsAndDataStructure
+//
+//  Created by Nikita Kechinov on 03.09.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
