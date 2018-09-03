eea389937ffa8df14cdbf346c026a062fa2969f0
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-09-03 13:25:41 +0300

    Queue data structure

---
+++ AlgorithmsAndDataStructure/QueueDataStructure/Queue.swift
@@ -0,0 +1,11 @@
+//
+//  Queue.swift
+//  AlgorithmsAndDataStructure
+//
+//  Created by Nikita Kechinov on 03.09.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import Foundation
+
+// Swift generic stack implementation example
