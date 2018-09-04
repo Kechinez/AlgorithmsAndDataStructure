77a2f36a5c43c692a73e53ccfd264788a1367936
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-09-04 10:53:00 +0300

    insertion sort algorithm

---
+++ AlgorithmsAndDataStructure/InsertionSortAlgorithm/InsertionSort.swift
@@ -0,0 +1,9 @@
+//
+//  InsertionSort.swift
+//  AlgorithmsAndDataStructure
+//
+//  Created by Nikita Kechinov on 04.09.2018.
+//  Copyright © 2018 Nikita Kechinov. All rights reserved.
+//
+
+import Foundation
