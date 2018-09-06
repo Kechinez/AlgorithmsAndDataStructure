d142d7fe1973b312541d4904ef103826501f1de7
Nikita Kechinov <nikitakechinov@Mac-mini-Nikita.local>
2018-09-06 12:04:10 +0300

    Insertion sort is done

--- AlgorithmsAndDataStructure/InsertionSortAlgorithm/InsertionSort.swift
+++ AlgorithmsAndDataStructure/InsertionSortAlgorithm/InsertionSort.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+
+func insertionSortWithSwapping(array: [Int]) -> ([Int], Double) {
+    let start = CFAbsoluteTimeGetCurrent()
+    var sortedArray = array
+    var counter = 0
+    for i in 1..<array.count {
+        var y = i
+        let temp = sortedArray[i]
+        while y > 0 && temp < sortedArray[y - 1] {
+            counter = counter + 1
+            sortedArray.swapAt(y - 1, y)
+            y -= 1
+        }
+    }
+    let diff = (CFAbsoluteTimeGetCurrent() - start) * 1000
+
+    return (sortedArray, diff)
+
+}
+
+
+
+func insertionSortWithoutSwapping(array: [Int]) -> ([Int], Double) {
+    let start = CFAbsoluteTimeGetCurrent()
+    var sortedArray = array
+    for i in 1..<array.count {
+        var y = i
+        let temp = sortedArray[i]
+        while y > 0 && temp < sortedArray[y - 1] {
+            sortedArray[y] = sortedArray[y - 1]
+            y -= 1
+        }
+        sortedArray[y] = temp
+    }
+    
+    let diff = (CFAbsoluteTimeGetCurrent() - start) * 1000
+    return (sortedArray, diff)
+}
+
+
+
--- AlgorithmsAndDataStructure/main.swift
+++ AlgorithmsAndDataStructure/main.swift
@@ -8,14 +8,24 @@
 
 import Foundation
 
-var stack = Stack<String>()
+//var stack = Stack<String>()
+//
+//stack.push(newElement: "One")
+//stack.push(newElement: "Two")
+//stack.push(newElement: "Three")
+//stack.push(newElement: "Four")
+//stack.push(newElement: "Five")
+//
+//print(stack.description)
+//stack.pop()
+//print(stack.description)
 
-stack.push(newElement: "One")
-stack.push(newElement: "Two")
-stack.push(newElement: "Three")
-stack.push(newElement: "Four")
-stack.push(newElement: "Five")
 
-print(stack.description)
-stack.pop()
-print(stack.description)
+
+//let array = [45, 32, 100, 9, 23, 26, 5, 7, 2, 0, 234, 54, 3, 1, 73, 79, 8, 25, 49, 6, 103, 85, 82, 74, 55, 4, 456, 234, 80, 37, 62, 61, 99, 18, 27, 13, 96, 78]
+
+
+
+
+
+
